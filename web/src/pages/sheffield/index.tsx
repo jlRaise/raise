f9b12cbc3db422d08713c63465ffe2a2ac7644d1
jlRaise <125690670+jlRaise@users.noreply.github.com>
2023-02-17 18:18:47 +0000

    Update index.tsx
    <Button variant="outline" size="large" className="mt-4 mb-12" href="donate/">Donate</Button>

--- web/src/pages/sheffield/index.tsx
+++ web/src/pages/sheffield/index.tsx
@@ -47,6 +47,7 @@ const IndexPage = () => (
             protected: convert.moneyToPeopleProtected("gbp", 489175),
           }}
         />
+        <Button variant="outline" size="large" className="mt-4 mb-12" href="donate/">Donate</Button>
       </Section>
       <CTADown text="How it works" href="#how-it-works" />
     </Cover>
